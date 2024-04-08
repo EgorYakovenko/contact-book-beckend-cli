0effeda108c1986bf6acf5c7c4e5e9c618eaaf69
EgorYakovenko <Egor.Yakovenko@ukr.net>
2024-04-09 01:02:21 +0200

    finish

---
+++ index.js
@@ -0,0 +1,40 @@
+const { program } = require('commander');
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse();
+
+const options = program.opts();
+
+const contacts = require('./contacts');
+
+// TODO: рефакторити
+async function invokeAction({ action, id, name, email, phone }) {
+  console.log(action);
+  switch (action) {
+    case 'list':
+      const allContacts = await contacts.listContacts();
+      return console.log(allContacts);
+
+    case 'get':
+      const contactById = await contacts.getContactById(id);
+      return console.log(contactById);
+
+    case 'add':
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.log(newContact);
+
+    case 'remove':
+      const removeContact = await contacts.removeContact(id);
+      return console.log(removeContact);
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(options);
